013e08f0cfbe40411537c6fd4e4316eb9af5f460
Luca Ferretti <elle.uca@gmail.com>
2021-03-10 09:33:48 +0100

    try codacy coverage report

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule KV.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.xml": :test
       ]
     ]
   end
