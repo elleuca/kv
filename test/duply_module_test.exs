cac8feeba0648288738930fd1829801ae5739c23
Luca Ferretti <elle.uca@gmail.com>
2021-03-10 10:40:28 +0100

    fix

--- test/duply_module_test.exs
+++ test/duply_module_test.exs
@@ -1,5 +1,7 @@
-defmodule MathModuleTest do
+defmodule DuplyModuleTest do
     use ExUnit.Case
+
+    import DuplyModule, only: [add: 2, subtract: 2]
 
     test "Subtract a zero from a zero" do
         assert subtract(0, 0) == 0
